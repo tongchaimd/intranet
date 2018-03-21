a9d76beb5d5ea67295e130f563318c11ee86c856
Tongchai Laoverakul <tongchaimd@gmail.com>
2018-03-21 17:07:33 +0800

    Add saving file method for News using gridfs

--- news/news.js
+++ news/news.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const gfs = require('gridfs-stream')(mongoose.connection.db, mongoose.mongo);
 const mongoosePaginate = require('mongoose-paginate');
 
 const newsSchema = new mongoose.Schema({
@@ -29,6 +30,22 @@ newsSchema.methods.getImageSourceArray = function getImageSourceArray(start, end
 	start = start || 0;
 	end = end || this.images.length;
 	return this.images.slice(start, end).map(image => `data:${image.contentType};base64,${image.data.toString('base64')}`);
+};
+
+newsSchema.methods.saveFile = function saveFile(readStream, name) {
+	return new Promise((resolve, reject) => {
+		const writeStream = gfs.createWriteStream({ filename: name });
+		readStream.pipe(writeStream);
+
+		readStream.on('error', (err) => {
+			reject(err);
+		});
+
+		writeStream.on('close', (file) => {
+			console.log(file);
+			resolve(file);
+		});
+	};
 };
 
 newsSchema.plugin(mongoosePaginate);
