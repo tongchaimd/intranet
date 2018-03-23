16232389a89e174ff9dd93f287b8e5ad4f8c7b2d
Tongchai Laoverakul <tongchaimd@gmail.com>
2018-03-23 10:45:44 +0800

    Add default value to businessCard

--- business-card/business-card.js
+++ business-card/business-card.js
@@ -4,42 +4,54 @@ const mongoosePaginate = require('mongoose-paginate');
 const multiLangStringSchema =  new mongoose.Schema({
 	english: {
 		type: String,
+		default: '',
 	},
 	thai: {
 		type: String,
+		default: '',
 	},
 	chinese: {
 		type: String,
+		default: '',
 	},
 }, { _id: false });
 
 const businessCardSchema = new mongoose.Schema({
 	fullName: {
 		type: multiLangStringSchema,
+		default: {},
 	},
 	position: {
 		type: multiLangStringSchema,
+		default: {},
 	},
 	companyName: {
 		type: multiLangStringSchema,
+		default: {},
 	},
 	companyLocation: {
 		type: multiLangStringSchema,
+		default: {},
 	},
 	phone: {
 		type: String,
+		default: '',
 	},
 	mobile: {
 		type: String,
+		default: '',
 	},
 	fax: {
 		type: String,
+		default: '',
 	},
 	email: {
 		type: String,
+		default: '',
 	},
 	website: {
 		type: String,
+		default: '',
 	},
 }, { timestamps: true });
 
